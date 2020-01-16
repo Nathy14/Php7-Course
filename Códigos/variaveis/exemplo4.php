c68767601aa2fd7038cca2fe80de5d0be24e97dd
Natália Gama <natalia.gama.mattos@hotmail.com>
2020-01-15 23:06:55 -0300

    Aula de variáveis pré-definidas

---
+++ Códigos/variaveis/exemplo4.php
@@ -0,0 +1,19 @@
+<?php 
+//Variáveis super-globais, pré-definidas
+/*
+URL.php?user=123&b=user2
+Toda informação vinda de get e post é string, é necessário converter se precisar.
+
+URL = Uniform Resource Locator
+URI = Uniform Resource Identifier,parte de uma URL, exeplo http, https.
+
+Protocolo+dominio+patch(caminho do site)+querry string(parâmetros)
+*/
+$nome = (int)$_GET["user"]; //get possui todas as varáveis 
+var_dump($nome);
+
+$ip = $_SERVER["REMOTE_ADDR"]; //IP do usuário(ambiente)
+$nome_arquivo = $_SERVER["SCRIPT_NAME"]; //nome do arquivo
+
+ ?>
+
